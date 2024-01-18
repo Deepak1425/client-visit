e6bc44cc9ff1a796291405610ec006de201386d5
Anubhav Verma <85112638+Anubhavop@users.noreply.github.com>
2024-01-18 08:47:28 +0530

    addition of router for send otp

--- server/routes/router.js
+++ server/routes/router.js
@@ -5,6 +5,7 @@ const controllers = require("../controllers/usercontrollers");
 
 
 router.post("/user/register",controllers.userregister);
+router.post("/user/sendotp",controllers.userOtpSend);
 
 
 
