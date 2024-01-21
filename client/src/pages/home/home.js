748e4ba53d92ca77b1b42fdc6fdde9daaec673dd
Anubhav Verma <85112638+Anubhavop@users.noreply.github.com>
2024-01-21 20:26:52 +0530

    Add files via upload

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const home = () => {
+const Home = () => {
+
+  const navigate = useNavigate();
+
+  const userValid = () => {
+    let token = localStorage.getItem("userdbtoken");
+    if (token) {
+      console.log("user valid")
+    } else {
+      navigate("*")
+    }
+  }
+
+  useEffect(() => {
+    userValid();
+  }, [])
   return (
-    <div>home</div>
+    <div>hello home</div>
   )
 }
 
-export default home
+export default Home
