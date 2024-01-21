28854669567bca4272e4f6c46931bcc2c9b90d96
Anubhav Verma <85112638+Anubhavop@users.noreply.github.com>
2024-01-21 20:20:43 +0530

    addition of token in schema

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const SECRECT_KEY = "abcdefghijklmnop"
 
 
 const userSchema = new mongoose.Schema({
-    fname: {
-        type: String,
-        required: true,
-        trim: true
-    },
     email: {
         type: String,
         required: true,
@@ -19,24 +15,31 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6
-    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true,
+            }
+        }
+    ]
     
 });
 
 
+userSchema.methods.generateAuthtoken = async function(){
+    try {
+        let newtoken = jwt.sign({_id:this._id},SECRECT_KEY,{
+            expiresIn:"1d"
+        });
 
-// hash password
-userSchema.pre("save", async function (next) {
-    if (this.isModified("password")) {
-        this.password = await bcrypt.hash(this.password, 12);
+        this.tokens = this.tokens.concat({token:newtoken});
+        await this.save();
+        return newtoken;
+    } catch (error) {
+        res.status(400).json(error)
     }
-
-    next();
-});
+}
 
 
 const users = new mongoose.model("users", userSchema);
