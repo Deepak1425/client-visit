53d3a011d0a2e55924d8829f1966bf7b0e70f9eb
Anubhav Verma <85112638+Anubhavop@users.noreply.github.com>
2024-01-21 20:24:10 +0530

    addition of userlogin function

--- server/controllers/usercontrollers.js
+++ server/controllers/usercontrollers.js
@@ -13,34 +13,6 @@ const tarnsporter = nodemailer.createTransport({
 }) 
 
 
-exports.userregister = async (req, res) => {
-    const { fname, email, password } = req.body;
-
-    if (!fname || !email || !password) {
-        res.status(400).json({ error: "Please Enter All Input Data" })
-    }
-
-    try {
-        const presuer = await users.findOne({ email: email });
-
-        if (presuer) {
-            res.status(400).json({ error: "This User Allready exist in our db" })
-        } else {
-            const userregister = new users({
-                fname, email, password
-            });
-
-            // here password hasing
-
-            const storeData = await userregister.save();
-            res.status(200).json(storeData);
-        }
-    } catch (error) {
-        res.status(400).json({ error: "Invalid Details", error })
-    }
-
-};
-
 // user send otp
 exports.userOtpSend = async (req, res) => {
     const { email } = req.body;
@@ -116,5 +88,28 @@ exports.userOtpSend = async (req, res) => {
     }
 };
 
+exports.userLogin = async(req,res)=>{
+    const {email,otp} = req.body;
 
+    if(!otp || !email){
+        res.status(400).json({ error: "Please Enter Your OTP and email" })
+    }
+
+    try {
+        const otpverification = await userotp.findOne({email:email});
+
+        if(otpverification.otp === otp){
+            const preuser = await users.findOne({email:email});
+
+            // token generate
+            const token = await preuser.generateAuthtoken();
+           res.status(200).json({message:"User Login Succesfully Done",userToken:token});
+
+        }else{
+            res.status(400).json({error:"Invalid Otp"})
+        }
+    } catch (error) {
+        res.status(400).json({ error: "Invalid Details", error })
+    }
+};
 
