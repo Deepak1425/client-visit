a2793decd2446aef65b576f67c51f15aca9c184e
Anubhav Verma <85112638+Anubhavop@users.noreply.github.com>
2024-01-21 20:19:04 +0530

    addition of router for userlogin

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,8 +4,8 @@ const controllers = require("../controllers/usercontrollers");
 
 
 
-router.post("/user/register",controllers.userregister);
 router.post("/user/sendotp",controllers.userOtpSend);
+router.post("/user/login",controllers.userLogin);
 
 
 
